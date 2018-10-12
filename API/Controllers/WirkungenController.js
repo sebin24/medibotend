80309b03f157f4ab74bd7129aea7cb78f8ec11b5
sebini24 <sebi.nguyen@googlemail.com>
2018-10-12 17:03:18 +0200

    14

--- API/Controllers/WirkungenController.js
+++ API/Controllers/WirkungenController.js
@@ -4,7 +4,7 @@ var Nebenwirkungen = mongoose.model('Nebenwirkungen'); // Daten laden und als Va
 var Wechselwirkungen = mongoose.model('Wechselwirkungen'); // Daten laden und als Variable deklarieren
 var Warnung = "Mithilfe dieser Datenbank können Sie sich über Arzneimittel und deren Wechselwirkungen informieren. Die medizinische Wissenschaft entwickelt sich ständig weiter. Neue Informationen finden nur mit zeitlicher Verzögerung Eingang in diese Datenbank. Lesen Sie daher immer die aktuelle Gebrauchsinformation, die Ihrem Medikament beiliegt, vollständig durch und fragen Sie Ihren Arzt oder Apotheker. Die Informationen dieser Datenbank sind nicht vollständig. Nicht jede Information ist für jeden Patienten relevant. Die Datenbank ersetzt daher nicht den Arztbesuch und nicht die Beratung durch den Apotheker."
 
-exports.processRequest = function(req, res) {
+exports.processRequest = function(req, res) { //unterscheidung welche Aktion gebraucht wird
   if (req.body.queryResult.action == "bot_ww") {
     getWW(req,res)
   }
@@ -14,7 +14,7 @@ exports.processRequest = function(req, res) {
   }
 };
 
-function getNW(req,res)
+function getNW(req,res) // Nebenwirkungen in der DB suchen
 {
   let nwToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.Medikamente_Stoffe ? req.body.queryResult.parameters.Medikamente_Stoffe : 'Unknown';
   Nebenwirkungen.findOne({name:nwToSearch},function(err,result){
@@ -39,9 +39,9 @@ function getNW(req,res)
 
 }
 
-function getWW(req,res)
+function getWW(req,res) //Wechselwirkungen in der DB suchen (in der entwicklung)
 {
-  let wwToSearch = req.body.queryResult.parameters.Medikamente_Stoffe;
+  let wwToSearch = req.body.queryResult.parameters.Medikamente_Stoffe + "und" +req.body.queryResult.parameters.Medikamente_Stoffe1 || req.body.queryResult.parameters.Medikamente_Stoffe1 + "und" +req.body.queryResult.parameters.Medikamente_Stoffe;
   Wechselwirkungen.findOne({name: wwToSearch}, function(err,result){
     if (err){
         return res.json({
@@ -55,7 +55,7 @@ function getWW(req,res)
     }
     else {
       return res.json({
-        "fulfillmentText": "Ich habe keine Wechselwirkungen zu "+req.body.queryResult.parameters.Medikamente_Stoffe+
+        "fulfillmentText": "Ich habe keine Wechselwirkungen zu "+req.body.queryResult.parameters.Medikamente_Stoffe+"und"+req.body.queryResult.parameters.Medikamente_Stoffe1+
         " in meiner Datenbank gefunden."+ "\n\n" + Warnung
       });
     }
--- API/Routes/Routes.js
+++ API/Routes/Routes.js
@@ -4,7 +4,7 @@ module.exports = function(app) { // export der funktion zu index.js
   var WechselwirkungController = require('../Controllers/WirkungenController');
   var apiRoutes =  express.Router();
   app.get('/',function(req,res){ // bei Seiten aufruf ohne JSON-Datei/Anfangsnachricht
-    res.send('Webhook für die Wirkungen aktiv');
+    res.send('Webhook für die Wirkungen aktiv!');
   });
 
   app.route('/') // ansonsten WechselwirkungController ausführen
